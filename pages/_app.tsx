2f9ddd63588a8485e856c767e7ff544c5ea5d7e6
Luiiiz <52682525+meLuiiiz@users.noreply.github.com>
2021-07-24 05:14:12 -0300

    Troca dos arquivos de javascript para typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../src/css/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
+
+export default function Home() {
+  return (
+    <div>
+      <Title>Olá</Title>
+    </div>
+  )
+}

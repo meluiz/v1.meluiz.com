7165baeb2dc4bffd643b773e0d982d13f1d48aa8
Luiiiz <52682525+meLuiiiz@users.noreply.github.com>
2021-07-24 06:00:03 -0300

    Tipando novas variáveis para os temas

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -4,7 +4,26 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     title: string,
     colors: {
-      background: string
+      title: string,
+      background: string,
+      red: {
+        default: string,
+        lighter: string,
+        light: string,
+        dark: string,
+      },
+      blue: {
+        default: string,
+        lighter: string,
+        light: string,
+        dark: string,
+      },
+      yellow: {
+        default: string,
+        lighter: string,
+        light: string,
+        dark: string,
+      }
     },
     font: {
       size: string;

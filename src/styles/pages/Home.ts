305ca15a27bfb3e5439faa7d9196c34c526e25da
Luiiiz <52682525+meLuiiiz@users.noreply.github.com>
2021-07-25 05:48:36 -0300

    Estilização da página home (link)

---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const LinkBio = styled.a`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 8px 0;
+  color: ${props => props.theme.colors.text.link};
+  font-size: ${props => props.theme.font.size.md};
+  font-family: ${props => props.theme.font.family.sans};
+  font-weight: 600;
+
+  &:hover svg {
+    filter: brightness(.7);
+  }
+  
+  svg {
+    margin-right: 8px;
+    color: ${props => props.theme.colors.text.link};
+    filter: brightness(.5);
+  }
+`

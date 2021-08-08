0923157e50a9a1aeef74a1dccec16d4bfba37f58
Luiiiz <52682525+meLuiiiz@users.noreply.github.com>
2021-08-08 16:37:08 -0300

    Alterando diretório do favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="UTF-8" />
-          <link rel="shortcut icon" type="image/png" href="/assets/favicon/favicon.png" />
+          <link rel="shortcut icon" type="image/png" href="/img/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />

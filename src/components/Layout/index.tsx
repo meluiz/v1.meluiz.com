2ba98b7bdc5205d42539868008996cb03de0c07f
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2023-03-21 21:49:15 -0300

    correção da posição sticky

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,71 +11,76 @@ interface WrapperProps {
 }
 
 interface ContainerProps {
-  children?: React.ReactNode;
-  fluid?: boolean;
+  children?: React.ReactNode
+  fluid?: boolean
   size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl'
 }
 
 interface GridProps {
-  children?: React.ReactNode;
-  column?: boolean;
-  responsive?: boolean;
-  gap?: number;
-  align?: 'left' | 'center' | 'right';
-  justify?: 'left' | 'center' | 'right' | 'between';
+  children?: React.ReactNode
+  column?: boolean
+  responsive?: boolean
+  gap?: number
+  align?: 'left' | 'center' | 'right'
+  justify?: 'left' | 'center' | 'right' | 'between'
 }
 
-
 interface ColumnProps {
-  children?: React.ReactNode;
-  flex?: number;
-  styles?: CSSProperties;
+  sticky?: boolean
+  children?: React.ReactNode
+  flex?: number
+  styles?: CSSProperties
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
-  return (
-    <Base>
-      { children }
-    </Base>
-  )
+  return <Base>{children}</Base>
 }
 
 export const Container = ({ children, fluid = false, size }: ContainerProps) => {
   return (
     <Area fluid={fluid} size={size}>
-      { children }
+      {children}
     </Area>
   )
 }
 
 export const Grid = ({ children, gap, column, responsive, align, justify }: GridProps) => {
   const style = {
-    '--gap': `${gap !== undefined ? gap : 24}px`
+    '--gap': `${gap !== undefined ? gap : 24}px`,
   } as React.CSSProperties
 
   return (
-    <GridLayout column={column} responsive={responsive} align={align} justify={justify} style={style}>
-      { children }
+    <GridLayout
+      column={column}
+      responsive={responsive}
+      align={align}
+      justify={justify}
+      style={style}
+    >
+      {children}
     </GridLayout>
   )
 }
 
-export const Column = ({ children, flex, styles }: ColumnProps) => {
+export const Column = ({ children, flex, styles, sticky }: ColumnProps) => {
   const style = {
     '--flex': flex ? flex : 1,
-    ...styles
+    ...styles,
   } as React.CSSProperties
 
   return (
-    <ColumnLayout style={style}>
-      { children }
+    <ColumnLayout style={style} sticky={sticky}>
+      {children}
     </ColumnLayout>
   )
 }
 
 export const DribbbleProject = () => {
   return (
-    <DribbbleLink href="https://dribbble.com/shots/9809111-Developer-Dark-Grid-Portfolio" target="_blank">
+    <DribbbleLink
+      href="https://dribbble.com/shots/9809111-Developer-Dark-Grid-Portfolio"
+      target="_blank"
+    >
       <Dribbble size={32} />
     </DribbbleLink>
   )
